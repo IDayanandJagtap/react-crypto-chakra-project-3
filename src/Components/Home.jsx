cf7ee1b2ea94b6155a6c77f7cc3effaef8e1d6b9
Dayanand Jagtap <dayanandjagtap07@gmail.com>
2023-10-17 18:59:16 +0530

    Compress home image

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import { HStack, Image } from '@chakra-ui/react'
-import img from '../assets/btc.png'
-import { motion } from 'framer-motion'
+import React from "react";
+import { HStack, Image } from "@chakra-ui/react";
+import img from "../assets/btc_compressed.png";
+import { motion } from "framer-motion";
 const Home = () => {
-  return (
-    <HStack h={'85vh'} w={'full'} alignItems={'center'} justifyContent={'center'} >
-      <motion.div style={{ height: '100%' }} animate={{ translateY: '40px' }} transition={{
-        ease: "linear",
-        duration: 1,
-        repeat: Infinity,
-        repeatType: "reverse",
-      }}>
-        <Image src={img} w={'full'} h={'full'} objectFit={'contain'} />
-      </motion.div>
-    </HStack>
-  )
-}
+    return (
+        <HStack
+            h={"85vh"}
+            w={"full"}
+            alignItems={"center"}
+            justifyContent={"center"}
+        >
+            <motion.div
+                style={{ height: "100%" }}
+                animate={{ translateY: "40px" }}
+                transition={{
+                    ease: "linear",
+                    duration: 1,
+                    repeat: Infinity,
+                    repeatType: "reverse",
+                }}
+            >
+                <Image src={img} w={"full"} h={"full"} objectFit={"contain"} />
+            </motion.div>
+        </HStack>
+    );
+};
 
-export default Home
+export default Home;
